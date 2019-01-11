f679f85afb496778c31484e43baabacae2c29961
Laura Prata <diana.prata@fortech.ro>
2019-01-11 10:26:16 +0200

    no message

---
+++ app/src/test/java/ro/sample/myapplication/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package ro.sample.myapplication
+
+import org.amshove.kluent.shouldBeTrue
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun `this should be true`() {
+        true.shouldBeTrue()
+    }
+}
